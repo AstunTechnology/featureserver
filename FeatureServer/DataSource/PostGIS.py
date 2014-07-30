ad363320a23c325e4eedee1c95c26403dfecddd1
Peter Goulborn <petergoulborn@astuntechnology.com>
2014-07-30 16:55:35 +0100

    Added schema to PostGIS DataSource
    Also moved to using ''.format() for inserting values into strings.

--- FeatureServer/DataSource/PostGIS.py
+++ FeatureServer/DataSource/PostGIS.py
@@ -44,9 +44,13 @@ class PostGIS (DataSource):
                         'gte': '>=', 'lte': '<=',
                         'eq': '='}
 
-    def __init__(self, name, srid = 4326, srid_out = 4326, fid = "gid", geometry = "the_geom", fe_attributes = 'true', order = "", attribute_cols = '*', writable = True, encoding = "utf-8", hstore = 'false', hstore_attr = "", **args):
-        DataSource.__init__(self, name, **args)
-        self.table          = args["layer"]
+    def __init__(self, name, srid=4326, srid_out=4326, fid='gid',
+                 geometry='the_geom', fe_attributes='true', order='',
+                 attribute_cols='*', writable=True, encoding='utf-8',
+                 schema='public', hstore='false', hstore_attr='', **kwargs):
+        DataSource.__init__(self, name, **kwargs)
+        self.schema         = schema.strip('" ')
+        self.table          = kwargs["layer"].strip('" ')
         self.fid_col        = fid
         self.encoding       = encoding
         self.geom_col       = geometry
@@ -54,11 +58,11 @@ class PostGIS (DataSource):
         self.srid           = srid
         self.srid_out       = srid_out
         self.db             = None
-        self.dsn            = args["dsn"]
+        self.dsn            = kwargs["dsn"]
         self.writable       = writable
         self.attribute_cols = attribute_cols
-
         self.fe_attributes = True
+
         if fe_attributes.lower() == 'false':
             self.fe_attributes  = False
 
@@ -143,25 +147,27 @@ class PostGIS (DataSource):
         return seq
 
     def insert (self, action):
-        table = self.table
-        if '"' not in table:
-            table = '"{}"'.format(table)
         self.begin()
         if action.feature != None:
             feature = action.feature
-            columns = '"'+'", "'.join(self.column_names(feature)+[self.geom_col])+'"'
-            values = ", ".join(self.value_formats(feature)+["ST_SetSRID('%s'::geometry, %s) " % (WKT.to_wkt(feature.geometry), self.srid)])
+            column_arr = self.column_names(feature) + [self.geom_col]
+            columns = '"{}"'.format('", "'.join(column_arr))
+            srid_wkt = "ST_SetSRID('{}'::geometry, {}) ".format(
+                WKT.to_wkt(feature.geometry), self.srid)
+            value_arr = self.value_formats(feature) + [srid_wkt]
+            values = ", ".join(value_arr)
 
-            sql = "INSERT INTO %s (%s) VALUES (%s)" % (table, columns, values)
+            sql = 'INSERT INTO {}.{} ({}) VALUES ({})'.format(
+                self.schema, self.table, columns, values)
 
             cursor = self.db.cursor()
             logging.debug(sql)
             cursor.execute(str(sql), self.feature_values(feature))
 
-            cursor.execute("SELECT currval('%s');" % self.id_sequence())
+            cursor.execute("SELECT currval('{}');".format(self.id_sequence()))
             action.id = cursor.fetchone()[0]
 
-            return InsertResult(action.id, "")
+            return InsertResult(action.id, '')
 
         elif action.wfsrequest != None:
             sql = action.wfsrequest.getStatement(self)
@@ -173,27 +179,25 @@ class PostGIS (DataSource):
             cursor.execute("SELECT currval('%s');" % self.id_sequence())
             action.id = cursor.fetchone()[0]
 
-            return InsertResult(action.id, "")
+            return InsertResult(action.id, '')
 
         return None
 
 
     def update (self, action):
-        table = self.table
-        if '"' not in table:
-            table = '"{}"'.format(table)
         if action.feature != None:
             feature = action.feature
             predicates = ", ".join(self.feature_predicates(feature))
             attrs = self.feature_values(feature)
 
-            sql = "UPDATE %s SET %s WHERE %s = %d" % (table, predicates, self.fid_col, action.id)
+            sql = 'UPDATE "{}"."{}" SET {} WHERE {} = {}' % (
+                self.schema, self.table, predicates, self.fid_col, str(action.id))
 
             cursor = self.db.cursor()
             logging.debug(cursor.mogrify(sql, attrs))
             cursor.execute(sql, attrs)
 
-            return UpdateResult(action.id, "")
+            return UpdateResult(action.id, '')
 
         elif action.wfsrequest != None:
             sql = str(action.wfsrequest.getStatement(self))
@@ -202,23 +206,21 @@ class PostGIS (DataSource):
             logging.debug(sql)
             cursor.execute(sql)
 
-            return UpdateResult(action.id, "")
+            return UpdateResult(action.id, '')
 
         return None
 
     def delete (self, action):
-        table = self.table
-        if '"' not in table:
-            table = '"{}"'.format(table)
         attrs = {self.fid_col: action.id}
         if action.id != None:
-            sql = "DELETE FROM %s WHERE %s = %%(%s)s" % ( table, self.fid_col, self.fid_col )
+            sql = 'DELETE FROM "{0}"."{1}" WHERE {2} = %%({2})s'.format(
+                self.schema, self.table, self.fid_col )
 
             cursor = self.db.cursor()
             logging.debug(cursor.mogrify(sql, attrs))
             cursor.execute(sql, attrs)
 
-            return DeleteResult(action.id, "")
+            return DeleteResult(action.id, '')
 
         elif action.wfsrequest != None:
             sql = action.wfsrequest.getStatement(self)
@@ -227,38 +229,37 @@ class PostGIS (DataSource):
             logging.debug(cursor.mogrify(sql, attrs))
             cursor.execute(sql, attrs)
 
-            return DeleteResult(action.id, "")
+            return DeleteResult(action.id, '')
 
         return None
 
 
     def select (self, action):
         cursor = self.db.cursor()
-        table = self.table
-        if '"' not in table:
-            table = '"{}"'.format(table)
 
         if action.id is not None:
-            sql = "SELECT ST_AsText(ST_Transform(%s, %d)) as fs_text_geom, " % (self.geom_col, int(self.srid_out))
+            sql = ('SELECT ST_AsText(ST_Transform({0}, {1})) AS fs_text_geom, '
+                   ''.format(self.geom_col, self.srid_out))
 
             if hasattr(self, 'version'):
-                sql += "%s as version, " % self.version
+                sql += '{} as version, '.format(self.version)
             if hasattr(self, 'ele'):
-                sql += "%s as ele, " % self.ele
+                sql += '{} as ele, '.format(self.ele)
 
-            sql += "\"%s\"" % self.fid_col
+            sql += '"{0}"'.format(self.fid_col)
 
             if len(self.attribute_cols) > 0:
-                sql += ", %s" % self.attribute_cols
+                sql += ', {0}'.format(self.attribute_cols)
 
-            if hasattr(self, "additional_cols"):
+            if hasattr(self, 'additional_cols'):
                 cols = self.additional_cols.split(';')
-                additional_col = ",".join(cols)
-                sql += ", %s" % additional_col
+                additional_col = ','.join(cols)
+                sql += ', {0}'.format(additional_col)
             attrs = {self.fid_col: str(action.id)}
 
 
-            sql += " FROM %s WHERE %s = %%(%s)s" % (table, self.fid_col, self.fid_col)
+            sql += ' FROM "{0}"."{1}" WHERE {2} = %%({3})s'.format(
+                self.schema, self.table, self.fid_col, self.fid_col)
 
             logging.debug(cursor.mogrify(sql, attrs))
             cursor.execute(sql, attrs)
@@ -276,17 +277,23 @@ class PostGIS (DataSource):
                     else:
                         attrs[key] = value
             if action.bbox:
-                filters.append( "%s && ST_Transform(ST_SetSRID('BOX3D(%f %f,%f %f)'::box3d, %s), %s) AND ST_Intersects(%s, ST_Transform(ST_SetSRID('BOX3D(%f %f,%f %f)'::box3d, %s), %s))" % (
-                                        (self.geom_col,) + tuple(action.bbox) + (self.srid_out,) + (self.srid,) + (self.geom_col,) + (tuple(action.bbox) + (self.srid_out,) + (self.srid,))))
-            sql = "SELECT ST_AsText(ST_Transform(%s, %d)) as fs_text_geom, " % (self.geom_col, int(self.srid_out))
+                bbox_def = "'BOX3D({0})::box3d'".format(', '.join(action.bbox))
+                filters.append('ST_Intersects("{0}", ST_Transform('
+                               'ST_SetSRID({1}, {2}), {3}))'.format(
+                               self.geom_col, bbox_def, self.srid_out,
+                               self.srid))
+            cols = ['ST_AsText(ST_Transform("{0}", {1})) '
+                    'AS fs_text_geom'.format(self.geom_col, self.srid_out)]
             if hasattr(self, 'ele'):
-                sql += "%s as ele, " % self.ele
+                ele = '"{0}" AS ele'.format(self.ele)
+                cols.append(ele)
             if hasattr(self, 'version'):
-                sql += "%s as version, " % self.version
-            sql += "\"%s\"" % self.fid_col
+                version = '"{0}" AS version'.format(self.version)
+                cols.append(version)
+            cols.append('"{0}"'.format(self.fid_col))
 
             if len(self.attribute_cols) > 0:
-                sql += ", %s" % self.attribute_cols
+                cols.append(self.attribute_cols)
 
             # check OGC FE attributes
             if self.fe_attributes and action.wfsrequest:
@@ -296,42 +303,37 @@ class PostGIS (DataSource):
                 fe_cols = filter(lambda x: x not in ad_cols, fe_cols)
 
                 if len(fe_cols) > 0:
-                    sql += ", %s" % ",".join(fe_cols)
+                    cols.extend(fe_cols)
 
-            if hasattr(self, "additional_cols"):
-                cols = self.additional_cols.split(';')
-                additional_col = ",".join(cols)
-                sql += ", %s" % additional_col
+            if hasattr(self, 'additional_cols'):
+                cols.extend(self.additional_cols.split(';'))
 
-
-            sql += ' FROM %s' % (table)
+            cols_def = ', '.join(cols)
+            sql = 'SELECT {0} FROM "{1}"."{2}"'.format(cols_def, self.schema,
+                                                       self.table)
 
             if filters:
-                sql += " WHERE " + " AND ".join(filters)
+                sql += ' WHERE ' + ' AND '.join(filters)
+
             if action.wfsrequest:
-                if filters:
-                    sql += " AND "
-                else:
-                    sql += " WHERE "
-
-                sql += action.wfsrequest.render(self)
-
+                sql += ' AND ' if filters else ' WHERE '
+                sql += '{0}'.format(action.wfsrequest.render(self))
 
             if self.order:
-                sql += " ORDER BY " + self.order
+                sql += ' ORDER BY {0}'.format(self.order)
             if action.maxfeatures:
-                sql += " LIMIT %d" % action.maxfeatures
-            #else:
-            #    sql += " LIMIT 1000"
+                sql += ' LIMIT {0}'.format(str(action.maxfeatures))
             if action.startfeature:
-                sql += " OFFSET %d" % action.startfeature
+                sql += ' OFFSET {0}'.format(str(action.startfeature))
 
             try:
                 logging.debug(cursor.mogrify(sql, attrs))
-                cursor.execute(str(sql), attrs)
+                cursor.execute(sql, attrs)
             except Exception, e:
                 if e.pgcode[:2] == errorcodes.CLASS_SYNTAX_ERROR_OR_ACCESS_RULE_VIOLATION:
-                    raise InvalidValueException(**{'dump':e.pgerror,'layer':self.name,'locator':'PostGIS'})
+                    raise InvalidValueException(dump=e.pgerror,
+                                                layer=self.name,
+                                                locator='PostGIS')
 
             result = cursor.fetchall() # should use fetchmany(action.maxfeatures)
 
@@ -339,7 +341,8 @@ class PostGIS (DataSource):
         features = []
         for row in result:
             props = dict(zip(columns, row))
-            if not props['fs_text_geom']: continue
+            if not props['fs_text_geom']:
+                continue
             geom  = WKT.from_wkt(props['fs_text_geom'])
             id = props[self.fid_col]
             del props[self.fid_col]
@@ -349,7 +352,8 @@ class PostGIS (DataSource):
             for key, value in props.items():
                 if isinstance(value, str):
                         props[key] = unicode(value, self.encoding)
-                elif isinstance(value, datetime.datetime) or isinstance(value, datetime.date):
+                elif (isinstance(value, datetime.datetime)
+                      or isinstance(value, datetime.date)):
                     # stringify datetimes
                     props[key] = str(value)
 
@@ -360,14 +364,15 @@ class PostGIS (DataSource):
                     pass
 
             if (geom):
-                features.append( Feature( id, geom, self.geom_col, self.srid_out, props ) )
+                features.append(Feature(id, geom, self.geom_col,
+                                        self.srid_out, props))
         return features
 
     def getColumns(self):
         cols = []
 
         if hasattr(self, 'attribute_cols'):
-            cols = self.attribute_cols.split(",")
+            cols = self.attribute_cols.split(',')
 
         cols.append(self.geom_col)
         cols.append(self.fid_col)
